9f78adc4ea13cc4a2966c0323ae4092fb562a417
tanzhuo <zhuo_tan@sina.cn>
2017-03-02 16:04:39 +0800

    initial code push to remote.

---
+++ openredios/openredios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  openredios
+//
+//  Created by 谭卓 on 2017/3/2.
+//  Copyright © 2017年 谭卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ openredios/openredios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  openredios
+//
+//  Created by 谭卓 on 2017/3/2.
+//  Copyright © 2017年 谭卓. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
